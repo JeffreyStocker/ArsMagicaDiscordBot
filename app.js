07cbc98b7b718f1a5b5e19c64a9b89f5535ff6a3
Jeffrey Stocker <kagesennin@gmail.com>
2018-08-14 15:19:43 -0700

    seperated the command from being passed in to command

--- app.js
+++ app.js
@@ -20,45 +20,53 @@ var removeEmptyArraySpots = function (array) {
 
 var processContent = function (message) {
   var msg = message.content.trim();
-  var content = msg.split(' ');
-  removeEmptyArraySpots(content);
-  return content;
+  var [com, ...content] = msg.split(' ');
+  return [com, content.join('')];
 }
 
 client.on("message", (message) => {
-  var content = processContent(message);
+  var [com, content] = processContent(message);
 
-  if (!!content[0] && content[0].startsWith(process.env.COMMAND || '!')) {
-    content[0] = content[0].slice(1);
-    if (command.hasOwnProperty(content[0])) {
-      command[content[0]](message, content);
+  if (!!com && com.startsWith(process.env.COMMAND || '!')) {
+    com = com.slice(1);
+    if (command.hasOwnProperty(com)) {
+      try {
+        command[com](message, content);
+      } catch (err) {
+        console.log (err);
+      }
     }
   }
 });
 
 
 client.login(process.env.DISCORD_TOKEN)
-.then(()=> {
-  console.log ('Logged On');
+  .then(()=> {
+    console.log ('Logged On');
+  })
+  .catch(err => {
+    console.log ('Error: ', err)
+  });
+
+client.on('error', function (err) {
+  console.log (err)
 })
-.catch(err => {
-  console.log ('Error: ', err)
-});
 
 
 process.stdin.resume();
 
-var exitFunction = function (level) {
+var exitFunction = function (level, restart = false) {
   console.log('exiting at level:', level);
   client.destroy()
     .then (() => {
-      process.exit();
+      restart ? process.kill(process.pid, 'SIGUSR2') : process.exit();
     });
 }
 
 process.on('SIGUSR1', exitFunction.bind(this, 'SIGUSR1') );
-process.on('SIGUSR2', exitFunction.bind(this, 'SIGUSR2') );
+// process.on('SIGUSR2', exitFunction.bind(this, 'SIGUSR2') );
 process.on('SIGTERM', exitFunction.bind(this, 'SIGTERM') );
 process.on('SIGINT', exitFunction.bind(this, 'SIGINT') );
+// process.on('uncaughtException', exitFunction.bind(this, 'uncaughtException', true));
 
 // process.on('exit', exitFunction.bind(this, 'exit'))
