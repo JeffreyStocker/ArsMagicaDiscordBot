e5a6bc698f4d20338b99e5a40e581bc4416be4ab
Jeffrey Stocker <kagesennin@gmail.com>
2018-08-17 14:43:55 -0700

    added error catch console.logs to discordCommands

--- dicordCommands.js
+++ dicordCommands.js
@@ -1,25 +1,37 @@
 module.exports = {
   giveNotificationBack(messageObject, messageToReturn) {
-    messageObject.channel.send(messageObject.author + ' ' + messageToReturn);
+    messageObject.channel.send(messageObject.author + ' ' + messageToReturn).catch(err => {
+      console.log (err.message);
+    });
   },
 
   rolledMessage(messageObject, messageToReturn) {
-    messageObject.channel.send(messageObject.author + ' rolled: ' + messageObject.content.slice(1) + '\n' + messageToReturn);
+    messageObject.channel.send(messageObject.author + ' rolled: ' + messageObject.content.slice(1) + '\n' + messageToReturn).catch(err => {
+      console.log (err.message);
+    });
   },
 
   reply (messageObject, messageToReturn) {
-    messageObject.reply(messageToReturn);
+    messageObject.reply(messageToReturn).catch(err => {
+      console.log (err.message);
+    });
   },
 
   getNickname (messageObject) {
-    return messageObject.channel.members.get(messageObject.author.id).nickname;
+    return messageObject.channel.members.get(messageObject.author.id).nickname.catch(err => {
+      console.log (err.message);
+    });
   },
 
   sendMessage (messageObject, messageToReturn) {
-    messageObject.channel.send(messageToReturn);
+    messageObject.channel.send(messageToReturn).catch(err => {
+      console.log (err.message);
+    });
   },
 
   sendDm (messageObject, messageToReturn) {
-    messageObject.author.send(messageToReturn);
+    messageObject.author.send(messageToReturn).catch(err => {
+      console.log (err.message);
+    });
   }
 };
