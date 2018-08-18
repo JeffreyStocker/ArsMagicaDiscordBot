42f08bbc3dc680f1d3cdf07d829e432a23951b79
Jeffrey Stocker <kagesennin@gmail.com>
2018-08-17 17:32:09 -0700

    fixed formatting

--- pouch.js
+++ pouch.js
@@ -7,7 +7,7 @@ PouchDB.plugin(require('pouchdb-find'));
 module.exports = {
   findBetweenTime: function(startTime = new Date(), endTime = new Date()) {
     return new Promise ((resolve, revoke) => {
-      if (startTime.constructor.name !== 'Date' || endTime.constructor.name !== 'Date') { revoke(new Error('startTime and endTime must be Date Objects'))}
+      if (startTime.constructor.name !== 'Date' || endTime.constructor.name !== 'Date') { revoke(new Error('startTime and endTime must be Date Objects')); }
       if (startTime < endTime) {
         let temp = endTime;
         endTime = startTime;
@@ -18,14 +18,15 @@ module.exports = {
           timeStamp: { $lte: startTime, $gte: endTime }
         }
       }, (err, docs) => {
-        if (err) { revoke (err) }
-        else { resolve(docs.docs) }
+        if (err) {
+          revoke (err);
+        } else { resolve(docs.docs); }
       });
-    })
+    });
   },
 
   findBetweenTimePromise: function(startTime = new Date(), endTime = new Date()) {
-    if (startTime.constructor.name !== 'Date' || endTime.constructor.name !== 'Date') { revoke(new Error('startTime and endTime must be Date Objects'))}
+    if (startTime.constructor.name !== 'Date' || endTime.constructor.name !== 'Date') { revoke(new Error('startTime and endTime must be Date Objects')); }
     if (startTime < endTime) {
       let temp = endTime;
       endTime = startTime;
@@ -35,7 +36,7 @@ module.exports = {
       selector: {
         timeStamp: { $lte: startTime, $gte: endTime }
       }
-    })
+    });
   },
 
   insertAtTime: function (data) {
@@ -47,29 +48,29 @@ module.exports = {
           revoke(err);
         }
         resolve (doc);
-      })
-    })
+      });
+    });
   },
 
   getChar: function (userId, charName) {
     return new Promise((resolve, revoke) => {
       if (!userId || !charName) { revoke (new Error ('userId and charName must be supplied')); }
       module.exports.getUser(userId)
-      .then(( docs ) => {
+        .then(( docs ) => {
 
-        var character;
-        if (!docs || docs.length === 0) { resolve (null); }
-        characters = docs.characters;
-        if (!characters) { resolve (null); }
-        resolve(characters.find( char => {
-          if (char.name === charName) { return true }
-          return false;
-        }));
-      })
-      .catch (err => {
-        revoke(err);
-      });
-    })
+          var character;
+          if (!docs || docs.length === 0) { resolve (null); }
+          characters = docs.characters;
+          if (!characters) { resolve (null); }
+          resolve(characters.find( char => {
+            if (char.name === charName) { return true; }
+            return false;
+          }));
+        })
+        .catch (err => {
+          revoke(err);
+        });
+    });
   },
 
   getUser: function (_id) {
@@ -77,62 +78,64 @@ module.exports = {
   },
 
   findUser: function (_id) {
-    return db.find( {selector: {userId: _id}} )
+    return db.find( {selector: {userId: _id}} );
   },
+
+
 
   setChar: function (userId, charData) {
     var userData;
     db.get(userId)
-    .catch(err => {
-      if (err.status === 404) {
-        charData.index = 0;
-        return db.put({
-          _id: userId,
-          characters: [charData]
-        });
-      }
-      console.log (err);
-    })
-    .then(userData => {
-      if (!charData.index) {
-        charData.index = userData.characters.length;
-        userData.characters.push(charData);
-      } else {
-        userData.characters[charData.index];
-      }
-      return db.put(userData)
-    })
-    .then(results => {
-      console.log (results)
-    })
-    .catch(err => {
-      console.log (err)
-    });
+      .catch(err => {
+        if (err.status === 404) {
+          charData.index = 0;
+          return db.put({
+            _id: userId,
+            characters: [charData]
+          });
+        }
+        console.log (err);
+      })
+      .then(userData => {
+        if (!charData.index) {
+          charData.index = userData.characters.length;
+          userData.characters.push(charData);
+        } else {
+          userData.characters[charData.index];
+        }
+        return db.put(userData);
+      })
+      .then(results => {
+        console.log (results);
+      })
+      .catch(err => {
+        console.log (err);
+      });
   },
 
   listCharacters (userId) {
     return new Promise ((resolve, revoke) => {
       module.exports.getUser(userId)
-      .then(userData => {
-        resolve(userData.characters);
-       })
-      .catch(err => { revoke(err); });
+        .then(userData => {
+          resolve(userData.characters);
+        })
+        .catch(err => { revoke(err); });
     });
   },
 
   setUser: function (userId, charData) {
-    return db.put(userId, data)
+    return db.put(userId, data);
   },
 
 
-  count: function (){
+  count: function () {
     return db.info();
   },
 
   allDocs: function () {
     return db.allDocs({});
   }
-}
+};
 
 // module.exports.count().then(data => console.log(data))
 
@@ -147,7 +150,7 @@ var test = async function () {
   } catch (err) {
     console.log(err);
   }
-}
+};
 
 
 // test()
