10181ae74bb7a337fd5427409e9a3acd9110eb5f
Jeffrey Stocker <kagesennin@gmail.com>
2018-08-20 09:01:37 -0700

    set database commands for user for getuser and putUser

--- databaseUser.js
+++ databaseUser.js
@@ -1,14 +1,44 @@
 var PouchDB = require('pouchdb');
 var db = new PouchDB('./database/users');
 var moment = require('moment');
+const User = require ('./Users');
 
 PouchDB.plugin(require('pouchdb-find'));
 
+const postUser = function (name, id) {
+  var user = new User (name, id);
+  return new Promise ((resolve, revoke) => {
+    db.put(user)
+      .then ((info) => {
+        user._rev = info.rev;
+        resolve(user);
+      })
+      .catch ( revoke(err) );
+  });
+};
 
 const getUser = function (id) {
-  new Promise ((resolve, revoke) => {
+  return new Promise ((resolve, revoke) => {
     db.get(id)
-      .then (resolve)
+      .then (user => {
+        resolve(User.import(user));
+      })
+      .catch (err => {
+        if (err.status === 404) {
+          return revoke(null);
+        }
+        return revoke(err);
+      });
+  });
+};
+
+const putUser = function (user) {
+  return new Promise ((resolve, revoke) => {
+    db.put(user).catch(err => {
+      if (err.status === 409) {
+        return handleCollision(user);
+      }
+    }).then (resolve)
       .catch (err => {
         revoke(err);
       });
@@ -16,7 +46,18 @@ const getUser = function (id) {
 };
 
 
-
 module.exports = {
-  getUser
+  getUser,
+  putUser,
+};
+
+
+const handleCollision = function (doc) {
+  return new Promise ((resolve, revoke) => {
+    db.get(doc._id).then(returnedDoc => {
+      doc._rev = returnedDoc._rev;
+      return db.put(doc);
+    }).then (resolve)
+      .catch(revoke);
+  });
 };
