95318b728bb172359b16389e2225706365f36913
Jeffrey Stocker <kagesennin@gmail.com>
2018-08-20 14:52:52 -0700

    added spread operator to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -41,6 +41,7 @@ module.exports = {
     'one-var': [1, { 'uninitialized': 'always', 'initialized': 'never' }],
     /* Minuta */
     'comma-style': [2, 'last'],
-    'quotes': [1, 'single']
+    'quotes': [1, 'single'],
+    "experimentalObjectRestSpread": true
   }
 };
