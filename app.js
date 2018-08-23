9be853984da6edce42b9f686bf96c53b25aadb35
Jeffrey Stocker <kagesennin@gmail.com>
2018-08-23 11:50:05 -0700

    moved discord commands into discord.js

--- app.js
+++ app.js
@@ -1,61 +1,10 @@
 const dotenv = require ('dotenv').config();
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const path = require('path');
 
-const command = require ('./handleCommands');
+require(path.join(__dirname + '/database/databaseUser')).init();
+require(path.join(__dirname + '/database/databaseCharacter')).init();
 
-client.on('ready', () => {
-  console.log('I am ready!');
-});
-
-var removeEmptyArraySpots = function (array) {
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === '') {
-      array.splice(i, 1);
-      i--;
-    }
-  }
-  return array;
-};
-
-var processContent = function (message) {
-  var msg = message.content.trim();
-  var [com, ...content] = msg.split(' ');
-  return [com, content.join(' ')];
-};
-
-client.on('message', (message) => {
-  var [com, content] = processContent(message);
-
-  if (!!com && com.startsWith(process.env.COMMAND || '!')) {
-    com = com.slice(1).toLowerCase();
-    if (command.hasOwnProperty(com)) {
-      try {
-        command[com](message, content);
-      } catch (err) {
-        console.log (err);
-      }
-    }
-  }
-});
-
-
-client.login(process.env.DISCORD_TOKEN)
-  .then(()=> {
-    console.log ('Logged On');
-  })
-  .catch(err => {
-    console.log ('Error: ', err);
-  });
-
-client.on('error', function (err) {
-  if (err.message = 'read ECONNRESET') {
-    console.log('diconnected');
-  } else {
-    console.log (err.message);
-  }
-});
-
+const discord = require (path.join(__dirname + '/discord/discord.js'));
 
 process.stdin.resume();
 
