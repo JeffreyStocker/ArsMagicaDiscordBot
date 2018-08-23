9545e2ccaa2583b26a9375dad7d993bae9835e7e
Jeffrey Stocker <kagesennin@gmail.com>
2018-08-22 20:26:22 -0700

    bunch of changes, still wip

--- ars magica/Character.js
+++ ars magica/Character.js
@@ -1,45 +1,92 @@
+const getCorrectTech = function getCorrectTech (name) {
+  if (typeof name !== 'string') { throw new Error ('name should be a string'); }
+  name = name.toLowerCase();
+  let firstLetter = name[0];
 
-module.exports = class Character {
-  constructor(name, user, { attributes = {}, arts = {} } = {}) {
+};
+
+var wholeTech = {
+  creo: 0,
+  rego: 0,
+  perdo: 0,
+  muto: 0,
+  intelligo: 0
+};
+
+
+var wholeForms = {
+  animal: 0,
+  aquam: 0,
+  auram: 0,
+  corpus: 0,
+  herbam: 0,
+  imaginem: 0,
+  mentem: 0,
+  ignem: 0,
+  terram: 0,
+  vim: 0,
+};
+
+var techniques = {
+  cr: 0,
+  re: 0,
+  pe: 0,
+  mu: 0,
+  in: 0,
+};
+
+
+var forms = {
+  an: 0,
+  aq: 0,
+  au: 0,
+  co: 0,
+  he: 0,
+  im: 0,
+  ig: 0,
+  me: 0,
+  te: 0,
+  vi: 0,
+};
+
+var attributes = {
+  str: 0,
+  dex: 0,
+  com: 0,
+  pre: 0,
+  int: 0,
+  qui: 0,
+  sta: 0,
+};
+
+var test = [
+  '(cr)|(re)|(in)|(pe)|(mu)',
+  '(an)|(aq)|(au)|(co)|(he)|(im)|(ig)|(me)|(te)|(vi)'
+];
+
+const Character = class Character {
+  constructor(name, user, { attri = attributes, arts = {} } = {}) {
     if (!name || !user) {
       throw new Error ('Must include a name and userId');
     }
     this.name = name;
     this.user = user;
-    this.attribute = {
-      str: 0,
-      dex: 0,
-      com: 0,
-      pre: 0,
-      int: 0,
-      qui: 0,
-      sta: 0,
-    };
+    this.attributes = { ...attri };
     this.size = 0;
-    this.arts = {
-      cr: null,
-      re: null,
-      pe: null,
-      mu: null,
-      in: null,
-      an: null,
-      aq: null,
-      au: null,
-      co: null,
-      he: null,
-      ig: null,
-      im: null,
-      me: null,
-      te: null,
-      vi: null,
-    };
+    this.forms = { ... forms };
+    this.techniques = { ...techniques };
+
     attributes && this.setAllAttributes(attributes);
     arts && this.setAllAttributes(arts);
   }
 
+  get arts() {
+    return { ...this.forms, ...this.techniques};
+  }
+
   setAttribute(name, val) {
-    if (this.attribute[name]) {
-      this.attribute[name] = val;
+    if (this.attributes[name]) {
+      this.attributes[name] = val;
       return true;
     }
     return false;
@@ -65,6 +112,26 @@ module.exports = class Character {
     }
   }
 
+  set(stat, val) {
+    val = Number (val);
+    if ( isNaN(val)) { return null; }
+    if (this.forms.hasOwnProperty(stat)) {
+      this.forms[stat] = val;
+    } else if (this.techniques.hasOwnProperty(stat)) {
+      this.techniques[stat] = val;
+    } else if (this.attributes.hasOwnProperty(stat)) {
+      this.attributes[stat] = val;
+    } else {
+      return null;
+    }
+    return true;
+  }
+
+  static import (charData) {
+    charData.__proto__ = Character.prototype;
+    return charData;
+  }
+
   load (data) {
     for (let [key, val] of data.entries()) {
       this[key] = val;
@@ -79,3 +146,6 @@ module.exports = class Character {
     return output;
   }
 };
+
+
+module.exports = Character;
